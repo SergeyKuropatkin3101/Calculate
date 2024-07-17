67661f769af16bed6f9fdfbb8e7a7fde5e9e128b
SergeyKuropatkin3101 <sergeykuropatkin31012001@gmail.com>
2024-07-17 20:12:47 +0300

    Initial commit

--- app/src/main/java/com/example/c_0/MainActivity.kt
+++ app/src/main/java/com/example/c_0/MainActivity.kt
@@ -16,6 +16,7 @@ import android.widget.Toast
 import androidx.core.view.WindowInsetsCompat
 import android.content.SharedPreferences
 import android.content.Context
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     private lateinit var sharedpreferences: SharedPreferences
@@ -91,6 +92,7 @@ class MainActivity : AppCompatActivity() {
             editor.putString(KEY_RESULT_IN, result_in)
             editor.putString(KEY_RESULT_OUT, result_in)
             editor.apply()
+
             if (result_in.length >11){
                 textView_r.textSize = 30F
                 textView_r_out.textSize = 25F
@@ -121,7 +123,7 @@ class MainActivity : AppCompatActivity() {
 
         }
         textView_r.text = result_in
-        textView_r_out.text = result_in
+        textView_r_out.text = CalculatingTheResult()
 
     }
 
@@ -148,7 +150,8 @@ class MainActivity : AppCompatActivity() {
     fun fun_equally(view: View) {
         textView_r_out = findViewById(R.id.result_output)
         textView_r = findViewById(R.id.result)
-
+        val result = CalculatingTheResult()
+        val editor = sharedpreferences.edit()
 //        val scaleXAnimator = ObjectAnimator.ofFloat(textView_r_out, "translationY", 0.5f)
 //        val scaleYAnimator = ObjectAnimator.ofFloat(textView_r, "translationY", 0.5f)
 //
@@ -185,10 +188,161 @@ class MainActivity : AppCompatActivity() {
 
 
         var text_res = ""
-        textView_r.text = textView_r_out.text.toString()
+        textView_r.text = result
+        editor.putString(KEY_RESULT_IN, result)
+        editor.apply()
         textView_r_out.text = text_res
     }
 
+    private fun CalculatingTheResult(): String {
+        var result: String
+        var resultDouble = 0.0
+        var result_in = sharedpreferences.getString(KEY_RESULT_IN, "").toString()
+        if (result_in.isNotEmpty() ){
+            if (result_in.last() in "+-*/"){
+                result_in = result_in.substring(0,result_in.length-1)
+            }
+        }
+        var pos = 0
+        var pos1 = 0
+        var pos2 = 0
+        var k = 0.0
+        var k2 = 0.0
+        var index1: Int
+        var index2: Int
+        var FlagPos1: Boolean
+        var FlagPos2: Boolean
+        if(('*' in result_in) or ('/' in result_in)){
+            var count_oper = result_in.count{ (it == '/') or (it == '*') }
+//            Log.d("count_oper", count_oper.toString())
+            for (i in 0..count_oper-1){
+                pos = 0
+                k = 0.0
+                index1 = result_in.indexOf("*")
+                index2 = result_in.indexOf("/")
+                if(index1*index2>0){
+                    if (index1 <= index2){
+                        pos = index1
+                    }
+                    else{
+                        pos = index2
+                    }
+                }
+                else {
+                    if (index1 != -1) {
+                        pos = index1
+                    } else {
+                        pos = index2
+                    }
+                }
+                FlagPos1 = true
+                FlagPos2 = true
+//                Log.e("pos", pos.toString())
+                var j = 1
+                while(FlagPos1 or FlagPos2){
+                    if (FlagPos1 && ((pos-j) == 0)){
+                        k = result_in.substring(0,pos).toDouble()
+                        FlagPos1 = false
+                        pos1 = 0
+                    }
+                    if (FlagPos1 && (result_in[pos-j] in "+-*/")){
+                        k = result_in.substring(pos-j+1,pos).toDouble()
+                        FlagPos1 = false
+                        pos1 = pos-j+1
+                    }
+
+//                    Log.d("leingh", result_in.substring(pos+1,pos2))
+//                    Log.d("leingh12", FlagPos2.toString())
+//                    Log.d("leingh34", (pos2 == result_in.length).toString())
+
+                    if (FlagPos2 && (pos+j == result_in.length)){
+                        k2 = result_in.substring(pos+1,result_in.length).toDouble()
+                        FlagPos2 = false
+                        pos2 = pos+j
+                    }
+                    if (FlagPos2 && (result_in[pos+j] in "+-*/")){
+                        k2 = result_in.substring(pos+1,pos+j).toDouble()
+                        FlagPos2 = false
+                        pos2 = pos+j
+                    }
+                    j+=1
+                }
+                if (result_in[pos] == '*'){
+                    resultDouble = k*k2
+                }
+                else{
+                    if (k2 != 0.0){
+                        resultDouble = k/k2
+
+                    }
+                    else{
+                        return "Делить на 0 нельзя!"
+                    }
+                }
+//                if (result_in[pos1+1] in "+-*/"){
+//                    pos1 +=1
+//                }
+                Log.e("123",result_in.substring(0,pos1) )
+                Log.e("1233",result_in.substring(pos2))
+                result_in = result_in.substring(0,pos1)+resultDouble.toString()+result_in.substring(pos2)
+
+                Log.e("1236",result_in )
+            }
+        }
+            pos = 0
+            k = 0.0
+            index1 = result_in.indexOf("+")
+            index2 = result_in.indexOf("-")
+            if((index1 == -1) && (index2 == -1)){
+                return result_in
+            }
+            else{
+                if(index1*index2>0){
+                    if (index1 <= index2){
+                        pos = index1
+                    }
+                    else{
+                        pos = index2
+                    }
+                }
+                else{
+                    if (index1 != -1){
+                        pos = index1
+                    }
+                    else {
+                        pos = index2
+                    }
+                }
+            }
+            resultDouble = result_in.substring(0,pos).toDouble()
+            Log.d("mypos", result_in[pos].toString())
+            var i = pos+1  ///3+ 3* 3
+            while(i < result_in.length){
+                Log.e("my2", result_in[i].toString())
+                Log.e("my_i", i.toString())
+                Log.e("mylength", result_in.length.toString())
+                if ((result_in[i] in "+-") or (i == result_in.length-1)){
+                    if (i == result_in.length-1){
+                        i +=1
+                    }
+                    k = result_in.substring(pos+1,i).toDouble()
+                    Log.e("myk", k.toString())
+                    if (result_in[pos] == '+'){
+                        resultDouble +=k
+                        pos = i
+                    }
+                    else{
+                        resultDouble -= k
+                        pos = i
+                    }
+                }
+                i +=1
+            }
+
+        result = resultDouble.toString()
+        Log.d("myTag", result);
+        return result;
+    }
     fun del_text(v: View) {
         val editor = sharedpreferences.edit()
         var result_in = sharedpreferences.getString(KEY_RESULT_IN, "").toString()
@@ -219,7 +373,7 @@ class MainActivity : AppCompatActivity() {
 
 
             textView_r.text = result_in
-            textView_r_out.text = result_in
+            textView_r_out.text = CalculatingTheResult()
         }
 
 
